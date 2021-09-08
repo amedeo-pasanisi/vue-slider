fed4472f6ad28f3d7125874f66ecc44dc192db9f
amedeo <amedeopasanisi97@gmail.com>
2021-09-08 18:48:09 +0200

    click event

--- js/main.js
+++ js/main.js
@@ -4,10 +4,20 @@ const app = new Vue (
     {
         el: "#root",
         data: {
-            
+            counter: 0,
+            photos: [
+                "img/copertina1.jpg",
+                "img/hero-saline.jpg",
+                "img/ponza-i-panorami.jpg"
+            ]
         },
         methods: {
-            
+            increaseCounter () {
+                this.counter++
+            },
+            decreaseCounter () {
+                this.counter--
+            }
         }
     }
 )
